11f2af8500f028c68400a1ddcf52d61109e93797
Sina-moazzeni <sina.moazzeni96@gmail.com>
2025-02-24 08:59:07 +0330

    ارسال کل پروژه به گیت هاب

---
+++ src/i18n/resource.ts
@@ -0,0 +1,14 @@
+export const resources = {
+  en: {
+    translation: {
+      HOT: "HOT",
+      "Free Express Shipping": "Free Express Shipping",
+    },
+  },
+  fa: {
+    translation: {
+      HOT: "داغ",
+      "Free Express Shipping": "ارسال رایگان سریع",
+    },
+  },
+};
